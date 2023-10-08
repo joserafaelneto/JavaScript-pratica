7586e8f3118aace7e9a9c4dc6f384eeaeff3610a
Jose Rafael <134311039+joserafaelneto@users.noreply.github.com>
2023-10-08 10:38:15 -0300

    desafios da aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.1;
+console.log(`Velocidade permitida 60 Km/h.`);
+if(vel > 60){
+    console.log(`Sua velocidade foi de ${vel} Km/h, você será MULTADO!`);
+}else{
+    console.log(`Sua velocidade foi de ${vel} Km/h, você não será MULTADO!`);
+}
+console.log(`Dirija com atenção!`);
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Canadá';
+
+if(país == 'Brasil'){
+    console.log(`Se você nasceu no ${país}, você é BRASILEIRO!`);
+}else{
+    console.log(`Se você nasceu no ${país}, você é ESTRANGEIRO!`);
+}
